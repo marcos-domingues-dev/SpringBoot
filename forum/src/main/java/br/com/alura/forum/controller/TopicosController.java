c63f085d6cf66501bae02de47ca42f7721851f1c
Marcos Domingues <mdomingues.ti@gmail.com>
2020-08-23 16:16:06 -0300

    Spring Boot - Aula 02: Publicando Endpoints

---
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,23 @@
+package br.com.alura.forum.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+
+@RestController
+public class TopicosController {
+
+	@RequestMapping("/topicos")
+	public List<TopicoDto> listar() {
+		Topico topico = new Topico("Dúvida no package model", "Seria a pasta model", new Curso("Spring Boot", "Java"));
+		
+		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+	}
+	
+}
