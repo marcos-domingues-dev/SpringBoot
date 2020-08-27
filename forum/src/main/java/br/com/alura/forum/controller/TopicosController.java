c42803f34c38c7412161cf51e954d69ab17daac0
Marcos Domingues <mdomingues.ti@gmail.com>
2020-08-27 09:07:16 -0300

    Spring Boot - Aula 04: Trabalhando com POST

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,22 +1,37 @@
 package br.com.alura.forum.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
 
 	@Autowired
 	TopicoRepository topicoRepository;
 	
-	@RequestMapping("/topicos")
+	@Autowired
+	CursoRepository cursoRepository;
+	
+	
+	// --> O parâmetro "String nomeCurso" vêm na url, via método GET
+	
+	@GetMapping
 	public List<TopicoDto> listar(String nomeCurso) {
 		if (nomeCurso != null) {
 			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
@@ -27,4 +42,21 @@ public class TopicosController {
 		}
 	}
 	
+	
+	// --> O parâmetro vêm no corpo da requisição, via método POST
+	
+	@PostMapping
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
+		Topico topico = form.converter(cursoRepository);
+		topicoRepository.save(topico);
+		
+		URI uri = uriBuilder
+				.path("/topicos/{id}")
+				.buildAndExpand(topico.getId())
+				.toUri();
+		
+		return ResponseEntity
+				.created(uri) 
+				.body(new TopicoDto(topico)); // -> devolver o código 201
+	}
 }
